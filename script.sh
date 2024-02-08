7eb6b9b262a9a25d2f14ec792f4027faa32d985f
Christopher Jansson <Christopher.Jansson1@gmail.com>
2024-02-08 09:36:35 +0100

    asd

--- script.sh
+++ script.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
+
 # start by getting every file that has changed checking if the uppermost directory contains a helmchart
 GIT_REVISION=$(git rev-parse HEAD)
-git diff --name-only GIT_REVISION GIT_REVISION~1 | while read line 
+git diff --name-only $GIT_REVISION $GIT_REVISION~1 | while read CHART
 do
-# check if file is Chart.Yaml
+    # check if file is Chart.Yaml
+    if [ $(basename $CHART) == "Chart.yaml" ]; then
 
-if [ test -f basename $line == "Chart.yaml" ]; do
-    echo $line >> updated-helmcharts.txt
+        CHART_NAME=$(cat $CHART | awk -F "name: " 'NF>1{print $2 }')
+        CHART_VERSION=$(cat $CHART | awk -F "version: " 'NF>1{print $2 }')
 
-    CHART_NAME=$(cat $line | awk -F "name: " 'NF>1{print $2 }')
-    CHART_VERSION=$(cat $line | awk -F "version: " 'NF>1{print $2 }')
+        CHART_TAG=$CHART_NAME-$CHART_VERSION
+        CHART_DIR=$(dirname $CHART)
+        echo $CHART_TAG;
+        echo $CHART_DIR;
 
-    CHART_TAG=$CHART_NAME-$CHART_VERSION
 
-    # check if git tag exists for chart. Should be {Chart.name-chart.version}
-    if [ git rev-parse ${TAG} >/dev/null 2>&1 ]; then
-    echo "tag $TAG already exist"
-    else
-    updated-helmcharts.txt << $line
+        # check if git tag exists for chart. Should be {Chart.name-chart.version}
+        if [ git rev-parse ${TAG} >/dev/null 2>&1 ]; then
+            echo "chart version $TAG already exist, wont push helmchart ";
+            continue
+        fi
+
+        CHART_PACKAGE=$(helm package --version $CHART_VERSION -u $CHART_DIR | cut -d":" -f2 | tr -d '[:space:]')
+        echo $CHART_PACKAGE
+
+         if [ $(params.CUSTOM_CA_PATH) != "" ]; then
+            curl -u "${REG_USER}":"${REG_PASSWORD}" $(params.REGISTRY) --upload-file "$CHART_PACKAGE" --cacert $(params.CUSTOM_CA_PATH)
+        else
+            curl -u "${REG_USER}":"${REG_PASSWORD}" $(params.REGISTRY) --upload-file "$CHART_PACKAGE" 
+        fi
+
+        git tag -am "Bump version to $(CHART_TAG)" $(CHART_TAG)
+
     fi
-
-fi 
 
 done
 
-# remove duplicate entries
-sort updated-helmcharts.txt | uniq > updated-helmcharts.txt 
+git push --tags
